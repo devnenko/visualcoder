0698be81e1776671b1aba8a28099e5e2da18c4a2
nenko <78041991+devnenko@users.noreply.github.com>
2021-10-28 18:36:33 +0200

    init

---
+++ out/test.js
@@ -0,0 +1,4 @@
+console.log("hi");
+var elem = document.createElement('div');
+console.log(elem);
+document.body.appendChild(elem);
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+console.log("hi");
+const elem =document.createElement('div');
+console.log(elem);
+document.body.appendChild(elem);
+ 
