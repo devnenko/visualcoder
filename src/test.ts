99abdad8eb40bfb342b2e8d45d1954763dc2b5e1
nenko <78041991+devnenko@users.noreply.github.com>
2021-10-28 19:38:18 +0200

    testing out software layout

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,12 @@
-console.log("hi");
-const elem =document.createElement('div');
+
+console.log("hi2");
+window.addEventListener("contextmenu", e => e.preventDefault());
+var elem = document.createElement('div');
 console.log(elem);
+elem.className="buttoncss"
 document.body.appendChild(elem);
+var elem2 = document.createElement('h1');
+elem2.textContent="hello"
+elem2.className="textcss"
+elem.appendChild(elem2);
  
