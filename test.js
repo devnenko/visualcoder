26ca27b18aab261888c2bdaa24501cf8ab87d908
devnenko <78041991+devnenko@users.noreply.github.com>
2021-11-09 13:42:30 +0100

    da

---
+++ test.js
@@ -0,0 +1,10 @@
+console.log("hi2");
+window.addEventListener("contextmenu", function (e) { return e.preventDefault(); });
+var elem = document.createElement('div');
+console.log(elem);
+elem.className = "buttoncss";
+document.body.appendChild(elem);
+var elem2 = document.createElement('h1');
+elem2.textContent = "hello";
+elem2.className = "textcss";
+elem.appendChild(elem2);
