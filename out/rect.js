a2dd87d447255a9a0104182f38eaab6791323a61
nenko <78041991+devnenko@users.noreply.github.com>
2021-11-13 23:45:34 +0100

    stretch and fixed movement now working :)

--- out/rect.js
+++ out/rect.js
@@ -1,21 +1,17 @@
-class EdgesNullable {
+class EdgesBool {
     constructor(obj) {
-        this.top = obj.top || null;
-        this.bottom = obj.bottom || null;
-        this.left = obj.left || null;
-        this.right = obj.right || null;
-        if (obj.left == 0) {
-            this.left = 0;
-        }
-        if (obj.right == 0) {
-            this.right = 0;
-        }
-        if (obj.top == 0) {
-            this.top = 0;
-        }
-        if (obj.bottom == 0) {
-            this.bottom = 0;
-        }
+        this.top = obj.top || false;
+        this.bottom = obj.bottom || false;
+        this.left = obj.left || false;
+        this.right = obj.right || false;
+    }
+}
+class Edges {
+    constructor(obj) {
+        this.top = obj.top || 0;
+        this.bottom = obj.bottom || 0;
+        this.left = obj.left || 0;
+        this.right = obj.right || 0;
     }
     static getArrays(obj) {
         let arrayNames = [];
@@ -39,78 +35,81 @@ class EdgesNullable {
         return { nameArray: arrayNames, numberArray: arrayNumbers };
     }
     static add(obj1, obj2) {
-        //works only when obj1 doesnt have any null values
-        let res = new EdgesNullable({});
-        let arrays = EdgesNullable.getArrays(obj1);
-        if (obj2.left != null) {
-            res.left = obj1.left + obj2.left;
-        }
-        else {
-            res.left = obj1.left;
-        }
-        if (obj2.right != null) {
-            res.right = obj1.right + obj2.right;
-        }
-        else {
-            res.right = obj1.right;
-        }
-        if (obj2.top != null) {
-            res.top = obj1.right + obj2.top;
-        }
-        else {
-            res.top = obj1.top;
-        }
-        if (obj2.bottom != null) {
-            res.bottom = obj1.bottom + obj2.bottom;
-        }
-        else {
-            res.bottom = obj1.bottom;
-        }
-        return res;
+        return new Edges({
+            left: obj1.left + obj2.left,
+            right: obj1.right + obj2.right,
+            top: obj1.top + obj2.top,
+            bottom: obj1.bottom + obj2.bottom,
+        });
+    }
+    static screenEdges(canvas) {
+        return new Edges({
+            left: 0,
+            right: canvas.canvas.width,
+            top: 0,
+            bottom: canvas.canvas.height,
+        });
     }
 }
 export class Rect {
     constructor() {
-        this.parent = null;
+        //needed data
         this.canvas = null;
+        //final render data
         this.absPos = { x: 0, y: 0 };
         this.absSize = { w: 100, h: 100 };
-        this.relPos = { x: 80, y: 0 };
-        this.relSize = { w: 40, h: 100 };
-        this.absEdges = new EdgesNullable({});
-        this.relEdges = new EdgesNullable({});
-        this.margin = new EdgesNullable({});
-        this.fixedOffset = new EdgesNullable({});
         this.color = "pink";
-        this.layer = 0;
+        //for relative calculations
+        this.parent = null;
+        this.absEdges = new Edges({});
+        this.relEdges = new Edges({});
+        //addit data
+        this.stretchTo = new EdgesBool({});
+        this.fixedOffset = new Edges({});
+    }
+    setParent(parent) {
+        this.parent = parent;
+    }
+    setStretchTo(obj) {
+        this.stretchTo = new EdgesBool(obj);
+        this.resize();
+    }
+    setFixedOffset(obj) {
+        this.fixedOffset = new Edges(obj);
+        this.resize();
     }
     resize() {
         if (this.parent != null) {
-            if (this.margin.left != null && this.fixedOffset.left != null) {
-                console.log("Error: left of object defined 2 times:");
-                console.log(this);
+            if (this.stretchTo.left == true) {
+                this.absEdges.left = this.parent.absEdges.left;
             }
-            else if (this.margin.left != null) {
-                this.absEdges.left = this.parent.absEdges.left + this.margin.left;
+            if (this.stretchTo.right == true) {
+                this.absEdges.right = this.parent.absEdges.right;
             }
-            else if (this.fixedOffset.left != null) {
-                this.absEdges.left = this.parent.absEdges.left + this.fixedOffset.left;
+            if (this.stretchTo.top == true) {
+                this.absEdges.top = this.parent.absEdges.top;
             }
-            else {
-                console.log("Error: left of object not defined:");
-                console.log(this);
+            if (this.stretchTo.bottom == true) {
+                this.absEdges.bottom = this.parent.absEdges.bottom;
             }
-            this.absEdges = EdgesNullable.add(this.parent.absEdges, this.relEdges);
+            if (this.fixedOffset.left != 0) {
+                this.absEdges.left = this.parent.absEdges.right - this.fixedOffset.left;
+            }
+            if (this.fixedOffset.right != 0) {
+                this.absEdges.right = this.absEdges.left + this.fixedOffset.right;
+            }
+            if (this.fixedOffset.top != 0) {
+                this.absEdges.top = this.parent.absEdges.bottom - this.fixedOffset.top;
+            }
+            if (this.fixedOffset.bottom != 0) {
+                this.absEdges.bottom = this.absEdges.top + this.fixedOffset.bottom;
+            }
         }
         else {
-            this.absEdges = new EdgesNullable({ left: 0, right: this.canvas.canvas.width, top: 0, bottom: this.canvas.canvas.height });
+            this.absEdges = Edges.screenEdges(this.canvas);
         }
-        this.setEdgesWorld(this.absEdges);
-    }
-    zawardo(edges) {
-    }
-    setEdgesWorld(edges) {
-        let arrays = EdgesNullable.getArrays(edges);
+        //convert absolute edges to position and size
+        let arrays = Edges.getArrays(this.absEdges);
         for (var i = 0; i < arrays.nameArray.length; i++) {
             let name = arrays.nameArray[i];
             let value = arrays.numberArray[i];
@@ -131,35 +130,5 @@ export class Rect {
                     break;
             }
         }
-    }
-    setParent(parent) {
-        this.parent = parent;
-    }
-    setMargin(margin) {
-        this.margin = new EdgesNullable(margin);
-        let arrays = EdgesNullable.getArrays(this.margin);
-        for (var i = 0; i < arrays.nameArray.length; i++) {
-            let name = arrays.nameArray[i];
-            let value = arrays.numberArray[i];
-            switch (name) {
-                case 'left':
-                    this.relEdges.left = value;
-                    break;
-                case 'right':
-                    this.relEdges.right = -value;
-                    //this.setEdgesWorld({right:(this.canvas as Canvas).canvas.width-value})
-                    break;
-                case 'top':
-                    this.relEdges.top = value;
-                    //this.setEdgesWorld({top:value})
-                    break;
-                case 'bottom':
-                    this.relEdges.bottom = -value;
-                    //this.setEdgesWorld({bottom:(this.canvas as Canvas).canvas.height-value})
-                    break;
-            }
-        }
-        this.resize();
-        //this.updateMargin();
     }
 }
--- src/rect.ts
+++ src/rect.ts
@@ -1,15 +1,5 @@
 import { Canvas } from './canvas.js';
 
-interface Pos {
-    x:number;
-    y:number;
-}
-
-interface Size {
-    w:number;
-    h:number;
-}
-
 interface EdgesOpt {
     top?:number;
     bottom?:number;
@@ -17,32 +7,43 @@ interface EdgesOpt {
     right?:number;
 }
 
-class EdgesNullable {
-    top:number|null;
-    bottom:number|null;
-    left:number|null;
-    right:number|null;
+interface EdgesBoolOpt {
+    top?:true;
+    bottom?:true;
+    left?:true;
+    right?:true;
+}
+
+class EdgesBool {
+    top:boolean;
+    bottom:boolean;
+    left:boolean;
+    right:boolean;
+
+    constructor(obj: EdgesBoolOpt){
+        this.top=obj.top||false;
+        this.bottom=obj.bottom||false;
+        this.left=obj.left||false;
+        this.right=obj.right||false;
+    }
+}
+
+
+
+class Edges {
+    top:number;
+    bottom:number;
+    left:number;
+    right:number;
 
     constructor(obj: EdgesOpt){
-        this.top=obj.top||null;
-        this.bottom=obj.bottom||null;
-        this.left=obj.left||null;
-        this.right=obj.right||null;
-        if(obj.left==0){
-            this.left=0;
-        }
-        if(obj.right==0){
-            this.right=0;
-        }
-        if(obj.top==0){
-            this.top=0;
-        }
-        if(obj.bottom==0){
-            this.bottom=0;
-        }
+        this.top=obj.top||0;
+        this.bottom=obj.bottom||0;
+        this.left=obj.left||0;
+        this.right=obj.right||0;
     }
 
-    static getArrays(obj: EdgesNullable) {
+    static getArrays(obj: Edges) {
         let arrayNames=[]
         let arrayNumbers=[]
         if(obj.left!=null){
@@ -64,198 +65,94 @@ class EdgesNullable {
         return {nameArray:arrayNames,numberArray:arrayNumbers};
     }
 
-    static add(obj1:EdgesNullable,obj2:EdgesNullable){
-        //works only when obj1 doesnt have any null values
-        
-        let res:EdgesNullable=new EdgesNullable({});
-        let arrays=EdgesNullable.getArrays(obj1);
-        if(obj2.left!=null){
-            res.left=(obj1.left as number)+obj2.left;
-        }
-        else{
-            res.left=obj1.left;
-        }
+    static add(obj1:Edges,obj2:Edges){
+        return new Edges({
+            left:obj1.left+obj2.left,
+            right:obj1.right+obj2.right,
+            top:obj1.top+obj2.top,
+            bottom:obj1.bottom+obj2.bottom,
+        });
+    }
 
-        if(obj2.right!=null){
-            res.right=(obj1.right as number)+obj2.right;
-        }
-        else{
-            res.right=obj1.right;
-        }
-
-        if(obj2.top!=null){
-            res.top=(obj1.right as number)+obj2.top;
-        }
-        else{
-            res.top=obj1.top;
-        }
-
-        if(obj2.bottom!=null){
-            res.bottom=(obj1.bottom as number)+obj2.bottom;
-        }
-        else{
-            res.bottom=obj1.bottom;
-        }
-        return res;
+    static screenEdges(canvas:Canvas){
+        return new Edges({
+            left:0,
+            right:canvas.canvas.width,
+            top:0,
+            bottom:canvas.canvas.height,
+        });
     }
 }
 
 export class Rect {
-    public parent:Rect|null=null;
+    //needed data
     public canvas:Canvas|null=null;
-    public absPos:Pos={x:0,y:0};
-    public absSize:Size={w:100,h:100};
 
-    public relPos={x:80,y:0};
-    public relSize:Size={w:40,h:100};
-
-    absEdges:EdgesNullable=new EdgesNullable({});
-    relEdges:EdgesNullable=new EdgesNullable({});
-
-    private margin:EdgesNullable=new EdgesNullable({});
-    private fixedOffset:EdgesNullable=new EdgesNullable({});
-
+    //final render data
+    public absPos={x:0,y:0};
+    public absSize={w:100,h:100};
     public color="pink";
-    private layer=0;
+
+    //for relative calculations
+    public parent:Rect|null=null;
+    public absEdges:Edges=new Edges({});
+    public relEdges:Edges=new Edges({});
+
+    //addit data
+    public stretchTo:EdgesBool=new EdgesBool({});
+    public fixedOffset:Edges=new Edges({});
 
     constructor() {
-    }
-
-    public resize(){
-        if(this.parent!=null){
-            if(this.margin.left!=null&&this.fixedOffset.left!=null){
-                console.log("Error: left of object defined 2 times:")
-                console.log(this)
-            }
-            else if(this.margin.left!=null){
-                this.absEdges.left=(this.parent.absEdges.left as number)+this.margin.left;
-            }
-            else if(this.fixedOffset.left!=null){
-                this.absEdges.left=(this.parent.absEdges.left as number)+this.fixedOffset.left;
-            }
-            else{
-                console.log("Error: left of object not defined:")
-                console.log(this)
-            }
-
-            
-            this.absEdges=EdgesNullable.add(this.parent.absEdges,this.relEdges)
-        }
-        else{
-            this.absEdges=new EdgesNullable({left:0,right:(this.canvas as Canvas).canvas.width,top:0,bottom:(this.canvas as Canvas).canvas.height});
-        }
-        this.setEdgesWorld(this.absEdges);
-    }
-
-    public zawardo(edges:EdgesOpt){
-
-    }
-
-    public setEdgesWorld(edges:EdgesNullable){
-        let arrays=EdgesNullable.getArrays(edges);
-        for(var i =0;i<arrays.nameArray.length;i++){
-            let name=arrays.nameArray[i];
-            let value=arrays.numberArray[i];
-            switch (name){
-                case 'left':
-                    this.absSize.w=this.absSize.w-(value-this.absPos.x);
-                    this.absPos.x=value;
-                    break;
-                case 'right':
-                    this.absSize.w=value-this.absPos.x;
-                    break;
-                case 'top':
-                    this.absSize.h=this.absSize.h-(value-this.absPos.y);
-                    this.absPos.y=value;
-                    break;
-                case 'bottom':
-                    this.absSize.h=value-this.absPos.y;
-                    break;
-            }
-        }
     }
 
     public setParent(parent:Rect){
         this.parent=parent;
     }
 
-    public setMargin(margin:EdgesOpt){
-        this.margin=new EdgesNullable(margin);
-        let arrays=EdgesNullable.getArrays(this.margin);
-        for(var i =0;i<arrays.nameArray.length;i++){
-            let name=arrays.nameArray[i];
-            let value=arrays.numberArray[i];
-            switch (name){
-                case 'left':
-                    this.relEdges.left=value;
-                    break;
-                case 'right':
-                    this.relEdges.right=-value;
-                    //this.setEdgesWorld({right:(this.canvas as Canvas).canvas.width-value})
-                    break;
-                case 'top':
-                    this.relEdges.top=value;
-                    //this.setEdgesWorld({top:value})
-                    break;
-                case 'bottom':
-                    this.relEdges.bottom=-value;
-                    //this.setEdgesWorld({bottom:(this.canvas as Canvas).canvas.height-value})
-                    break;
-                }
-            }
+    public setStretchTo(obj:EdgesBoolOpt){
+        this.stretchTo=new EdgesBool(obj);
         this.resize();
-        //this.updateMargin();
     }
-/* JavaScript
-  code  
-    public updateMargin(){
-        let arrays=this.margin.getArrays();
-        for(var i =0;i<arrays.nameArray.length;i++){
-            let name=arrays.nameArray[i];
-            let value=arrays.numberArray[i];
-            switch (name){
-                case 'left':
-                    this.setEdgesWorld({left:value})
-                    break;
-                case 'right':
-                    this.setEdgesWorld({right:(this.canvas as Canvas).canvas.width-value})
-                    break;
-                case 'top':
-                    this.setEdgesWorld({top:value})
-                    break;
-                case 'bottom':
-                    this.setEdgesWorld({bottom:(this.canvas as Canvas).canvas.height-value})
-                    break;
-                }
+
+    public setFixedOffset(obj:EdgesOpt){
+        this.fixedOffset=new Edges(obj);
+        this.resize();
+    }
+
+    public resize(){
+        if(this.parent!=null){
+            if(this.stretchTo.left==true){
+                this.absEdges.left=this.parent.absEdges.left;
+            }
+            if(this.stretchTo.right==true){
+                this.absEdges.right=this.parent.absEdges.right;
+            }
+            if(this.stretchTo.top==true){
+                this.absEdges.top=this.parent.absEdges.top;
+            }
+            if(this.stretchTo.bottom==true){
+                this.absEdges.bottom=this.parent.absEdges.bottom;
+            }
+
+            if(this.fixedOffset.left!=0){
+                this.absEdges.left=this.parent.absEdges.right-this.fixedOffset.left;
+            }
+            if(this.fixedOffset.right!=0){
+                this.absEdges.right=this.absEdges.left+this.fixedOffset.right;
+            }
+            if(this.fixedOffset.top!=0){
+                this.absEdges.top=this.parent.absEdges.bottom-this.fixedOffset.top;
+            }
+            if(this.fixedOffset.bottom!=0){
+                this.absEdges.bottom=this.absEdges.top+this.fixedOffset.bottom;
             }
         }
-    }
-
-    
-    public setFixedOffset(fixedOffset:EdgesOpt){
-        //probably doesnt work for left  and top
-        this.fixedOffset=new EdgesNullable(fixedOffset);
-        this.updateFixedOffset();
-    }
-
-
-    public updateFixedOffset(){
-        if(this.fixedOffset.left!=null){
-            this.setEdgesWorld({left:this.fixedOffset.left})
+        else{
+            this.absEdges=Edges.screenEdges(this.canvas as Canvas);
         }
-        if(this.fixedOffset.right!=null){
-            this.setEdgesWorld({right:this.fixedOffset.right})
-        }
-        if(this.fixedOffset.top!=null){
-            this.setEdgesWorld({top:this.fixedOffset.top})
-        }
-        if(this.fixedOffset.bottom!=null){
-            this.setEdgesWorld({bottom:this.fixedOffset.bottom})
-        }
-    }
 
-    public setEdgesWorld(edges:EdgesOpt){
-        let arrays=new EdgesNullable(edges).getArrays();
+        //convert absolute edges to position and size
+        let arrays=Edges.getArrays(this.absEdges);
         for(var i =0;i<arrays.nameArray.length;i++){
             let name=arrays.nameArray[i];
             let value=arrays.numberArray[i];
@@ -277,5 +174,4 @@ export class Rect {
             }
         }
     }
-    */
 }
